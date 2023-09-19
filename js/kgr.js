4945db600802de1a0e78e04b3af49799c7941423
ga8226 <ga8226@naver.com>
2023-09-19 21:40:19 +0900

    파일 다시 다 가져오고 확인

---
+++ js/kgr.js
@@ -0,0 +1,12 @@
+// const content = "안녕하세요 : ) \n 주니어 front-end 개발자 김가림입니다!";
+// const text = document.querySelector(".text");
+// let i = 0;
+// function typing(){
+//     let txt = content[i++];
+//     text.innerHTML += txt=== "\n" ? "<br/>": txt;
+//     if (i > content.length) {
+//         text.textContent = "";
+//         i = 0;
+//     }
+// }
+// setInterval(typing, 200)
